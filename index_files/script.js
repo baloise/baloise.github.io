98ed1f71775d12e916c855c5f582ab267be81009
Matthias Cullmann <matthias.cullmann@baloise.ch>
2014-10-27 15:06:35 +0100

    correct indention

--- index_files/script.js
+++ index_files/script.js
@@ -1,47 +1,47 @@
 function loadRepos(data) {
-				if(data == null) {
-					return $.getJSON('https://api.github.com/orgs/baloise/repos?type=all', function(data) {loadRepos(data)}) 
-				}
-				data.sort(function(item1, item2) {
-					return item1.name.localeCompare(item2.name);
-				})
-				var items = []
-				
-				$.each(data, function(index, repo) {
-					if (repo.name != 'baloise.github.io') {
-						items.push("<li class='repo'><a href='" + repo.html_url + "'><div class='repo-name'><h3 class='no-toc'>"
-								+ repo.name + "</h3><div class='repo-desc'>" + repo.description + "</div></div>"
-								+ '</a></li>')
-					}
-				});
-				
-				$('<ul/>', {
-					'class' : 'repos',
-					html : items.join('')
-				}).appendTo('#repos')
+	if(data == null) {
+		return $.getJSON('https://api.github.com/orgs/baloise/repos?type=all', function(data) {loadRepos(data)}) 
+	}
+	data.sort(function(item1, item2) {
+		return item1.name.localeCompare(item2.name);
+	})
+	var items = []
+	
+	$.each(data, function(index, repo) {
+		if (repo.name != 'baloise.github.io') {
+			items.push("<li class='repo'><a href='" + repo.html_url + "'><div class='repo-name'><h3 class='no-toc'>"
+					+ repo.name + "</h3><div class='repo-desc'>" + repo.description + "</div></div>"
+					+ '</a></li>')
+		}
+	});
+	
+	$('<ul/>', {
+		'class' : 'repos',
+		html : items.join('')
+	}).appendTo('#repos')
 }
 
 function loadMembers(data) {
-		if(data == null) {
-			return $.getJSON('https://api.github.com/orgs/baloise/public_members', function(data) {loadMembers(data)}) 
-		}
-		 data.sort(function(item1, item2) {
-			return item1.login.localeCompare(item2.login);
-		})
+	if(data == null) {
+		return $.getJSON('https://api.github.com/orgs/baloise/public_members', function(data) {loadMembers(data)}) 
+	}
+	 data.sort(function(item1, item2) {
+		return item1.login.localeCompare(item2.login);
+	})
 
-		var items = []
-		$.each(data, function(index, member) {
-			var avatar = member.avatar_url.split("?")[0]
-					+ '?d=http://baloise.github.io/nobody.jpg'
-			items.push("<li class='contributor'><a href='" + member.html_url
-					+ "'><img src='" + avatar
-					+ "'/><span class='contributor-name'>" + member.login
-					+ "</span></a></li>")
-		})
-		$('<ul/>', {
-			'class' : 'repos',
-			html : items.join('')
-		}).appendTo('#members')
+	var items = []
+	$.each(data, function(index, member) {
+		var avatar = member.avatar_url.split("?")[0]
+				+ '?d=http://baloise.github.io/nobody.jpg'
+		items.push("<li class='contributor'><a href='" + member.html_url
+				+ "'><img src='" + avatar
+				+ "'/><span class='contributor-name'>" + member.login
+				+ "</span></a></li>")
+	})
+	$('<ul/>', {
+		'class' : 'repos',
+		html : items.join('')
+	}).appendTo('#members')
 }
 
 function getParameterByName(name, url) {
